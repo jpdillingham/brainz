b67cbbf04847309f25246f4748e0a3ca6a1ee110
JP Dillingham <jpdillingham@gmail.com>
2019-02-28 20:33:58 -0600

    clean up

--- Brainz/Program.cs
+++ Brainz/Program.cs
@@ -13,10 +13,10 @@ namespace Brainz
     class Program
     {
         [Argument('a', "artist", "The name of the artist to search for.")]
-        private static string Artist { get; set; }
+        private static string Artist { get; set; } = "atmosphere";
 
         [Argument('l', "album", "The name of the album to search for.")]
-        private static string Album { get; set; }
+        private static string Album { get; set; } = "fishing";
 
         private static HttpClient Http { get; } = new HttpClient();
 
@@ -24,65 +24,60 @@ namespace Brainz
         private static string ApiRoot = @"https://musicbrainz.org/ws/2";
 
         private static Func<string, string> ArtistRequest => (artist) => $"{ApiRoot}/artist/?query={artist}&fmt=json";
-        //private static Func<Guid, string> ReleaseGroupRequest => (mbid) => $"{ApiRoot}/artist/{mbid}?inc=release-groups&fmt=json";
         private static Func<Guid, int, int, string> ReleaseGroupRequest => (mbid, offset, limit) => $"{ApiRoot}/release-group?artist={mbid}&offset={offset}&limit={limit}&fmt=json";
-
-
-        //private static Func<Guid, string> ReleaseRequest => (mbid) => $"{ApiRoot}/release-group/{mbid}?inc=releases+media&fmt=json";
-        //https://musicbrainz.org/ws/2/release?release-group=04066384-affd-32ce-9b6f-ac4a97c07671&offset=0&limit=25&fmt=json&inc=media
         private static Func<Guid, int, int, string> ReleaseRequest => (mbid, offset, limit) => $"{ApiRoot}/release?release-group={mbid}&offset={offset}&limit={limit}&inc=media&fmt=json";
         private static Func<Guid, string> RecordingRequest => (mbid) => $"{ApiRoot}/release/{mbid}?inc=recordings&fmt=json";
 
         static int Main(string[] args)
         {
-            Arguments.Populate();
+            //Arguments.Populate();
 
             Http.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
 
             return Task.Run(() => MainAsync(args)).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
+        private static async Task<T> Get<T>(string request)
+        {
+            var json = await Http.GetStringAsync(request).ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
         private static async Task<int> MainAsync(string[] args)
         {
-            var req = ArtistRequest(Artist);
             Console.WriteLine($"Fetching artist matches for '{Artist}'...");
-            Console.WriteLine();
 
-            var artistJson = await Http.GetStringAsync(req).ConfigureAwait(false);
-            var artistResponse = JsonConvert.DeserializeObject<ArtistResponse>(artistJson);
-            var artists = artistResponse.Artists.OrderByDescending(a => a.Score);
+            var artists = (await Get<ArtistResponse>(ArtistRequest(Artist))).Artists
+                .OrderByDescending(a => a.Score);
 
             var bestArtist = artists.FirstOrDefault();
 
-            foreach (var artist in artistResponse.Artists)
+            foreach (var artist in artists.Take(10))
             {
-                Console.WriteLine($"{(artist.Name == bestArtist.Name ? "-->" : "   ")} {artist.Score.ToString().PadLeft(3)}%   {artist.Name}");
+                var disambiguation = string.IsNullOrEmpty(artist.Disambiguation) ? string.Empty : $"({artist.Disambiguation})";
+                Console.WriteLine($"{(artist.Id == bestArtist.Id ? "-->" : "   ")} {artist.Score.ToString().PadLeft(3)}%   {artist.Name} {disambiguation}");
             }
 
-            Console.WriteLine();
             Console.WriteLine($"Best artist match: {bestArtist.Name} (score: {bestArtist.Score}%)");
 
-            Console.WriteLine();
             Console.WriteLine($"Fetching release group matches for artist '{bestArtist.Name}', album '{Album}'...");
-            Console.WriteLine();
 
             List<ReleaseGroup> releaseGroups = new List<ReleaseGroup>();
             ReleaseGroupResponse releaseGroupResponse = null;
 
             do
             {
-                req = ReleaseGroupRequest(bestArtist.Id, releaseGroups.Count, 100);
-                var releaseGroupJson = await Http.GetStringAsync(req).ConfigureAwait(false);
-                releaseGroupResponse = JsonConvert.DeserializeObject<ReleaseGroupResponse>(releaseGroupJson);
-
+                releaseGroupResponse = await Get<ReleaseGroupResponse>(ReleaseGroupRequest(bestArtist.Id, releaseGroups.Count, 100));
                 releaseGroups.AddRange(releaseGroupResponse.ReleaseGroups);
             } while (releaseGroups.Count < releaseGroupResponse.ReleaseGroupCount);
 
-            releaseGroups = releaseGroups.OrderByDescending(r => r.Title.SimilarityCaseInsensitive(Album)).ToList();
+            releaseGroups = releaseGroups
+                .OrderByDescending(r => r.Title.SimilarityCaseInsensitive(Album))
+                .ToList();
 
             var bestReleaseGroup = releaseGroups.FirstOrDefault();
 
-            foreach (var releaseGroup in releaseGroups)
+            foreach (var releaseGroup in releaseGroups.Take(10))
             {
                 Console.WriteLine($"{(releaseGroup.Title == bestReleaseGroup.Title ? "-->" : "   ")} {(releaseGroup.Title.SimilarityCaseInsensitive(Album) * 100).ToString("F0").PadLeft(3)}%   {releaseGroup.Title}");
             }
@@ -99,6 +94,8 @@ namespace Brainz
             List<Release> releases = new List<Release>();
             ReleaseResponse releasesResponse = null;
 
+            string req;
+
             do
             {
                 req = ReleaseRequest(bestReleaseGroup.Id, 0, 100);
@@ -110,17 +107,18 @@ namespace Brainz
             
             releases = releases.OrderBy(r => r.Date.ToFuzzyDateTime()).ToList();
 
-            var bestRelease = releases
-                .Where(r => r.Status == "Official")
-                .Where(r => r.Country == "US")
-                .Where(r => string.IsNullOrEmpty(r.Disambiguation)).FirstOrDefault();
+            //var bestRelease = releases
+            //    .Where(r => r.Status == "Official")
+            //    .Where(r => r.Country == "US")
+            //    .Where(r => string.IsNullOrEmpty(r.Disambiguation)).FirstOrDefault();
+            var bestRelease = releases.FirstOrDefault();
 
             foreach (var release in releases)
             {
                 string trackStr = string.Join("+", release.Media.Select(m => m.TrackCount));
                 string mediaStr = string.Join("+", release.Media.Select(m => m.Format));
 
-                Console.WriteLine($"{(release.Id == bestRelease.Id ? "-->" : "   ")} {release.Title}\t{release.Country}\t{release.Date.PadRight(10)}\t{trackStr}\t{mediaStr}");
+                Console.WriteLine($"{(release.Id == bestRelease.Id ? "-->" : "   ")} {release.Title}\t{release.Country}\t{release.Date.PadRight(10)}\t{trackStr}\t{mediaStr}\t{release.Disambiguation}");
             }
 
             Console.WriteLine();
