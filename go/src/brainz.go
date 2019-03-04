dc492bb58bff92262e8ee817117d3b91f1fdda15
JP Dillingham <jpdillingham@gmail.com>
2019-03-03 18:54:10 -0600

    add api fetch

--- go/src/brainz.go
+++ go/src/brainz.go
@@ -4,12 +4,17 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	util "./util"
 )
 
 var stdin = bufio.NewReader(os.Stdin)
+
+var apiRoot = "https://musicbrainz.org/ws/2"
+var artistRequest = func(artist string) string { return apiRoot + "/artist/?query=" + artist + "&fmt=json" }
 
 func main() {
 	util.Logo()
@@ -18,6 +23,10 @@ func main() {
 
 	fmt.Println(artist)
 	fmt.Println(album)
+
+	fmt.Println(artistRequest(artist))
+
+	fmt.Println(httpGet(artistRequest(artist)))
 }
 
 func getInput() (string, string) {
@@ -26,4 +35,34 @@ func getInput() (string, string) {
 	flag.Parse()
 
 	return *artistPtr, *albumPtr
+}
+
+func httpGet(url string) (string, error) {
+	var client http.Client
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("User-Agent", "brainz/1.0.0 (https://github.com/jpdillingham/brainz)")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		bodyString := string(bodyBytes)
+		return bodyString, nil
+	}
+
+	return "", fmt.Errorf("MusicBrainz server returned status code %d", resp.StatusCode)
 }
