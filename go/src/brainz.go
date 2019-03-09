c6cd66f0bde65275c1d96dfbf81e04250de2c282
JP Dillingham <jpdillingham@gmail.com>
2019-03-09 16:49:58 -0600

    refactor canonical format code, add filtering of non-canonical releases

--- go/src/brainz.go
+++ go/src/brainz.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/url"
 	"sort"
+	"strings"
 
 	model "./model"
 	responses "./responses"
@@ -167,7 +169,7 @@ func getBestReleaseGroup(album string, mbid string) (bestReleaseGroup model.Rele
 }
 
 func getTrackList(mbid string) (tracks []model.Track) {
-	fmt.Printf("Fetching releases...\n\n")
+	fmt.Printf("Determining canonical track listing...\n\n")
 
 	response := responses.ReleaseResponse{}
 	var releases = []model.Release{}
@@ -192,12 +194,39 @@ func getTrackList(mbid string) (tracks []model.Track) {
 		}
 	}
 
+	canonicalFormat, canonicalTracks, formatErr := getCanonicalFormat(releases)
+
+	if formatErr == nil {
+		// filter non-canonical releases
+		fmt.Printf("\nCanonical format: %s, canonical track count: %s\n\n", canonicalFormat, canonicalTracks)
+
+		filteredReleases := []model.Release{}
+
+		for _, release := range releases {
+			releaseFormat, releaseTracks := release.MediaInfo()
+
+			if releaseFormat == canonicalFormat && releaseTracks == canonicalTracks {
+				filteredReleases = append(filteredReleases, release)
+				// date, _ := release.FuzzyDate()
+				// fmt.Printf("%s\t%s\n", date, release.DisambiguatedTitle())
+			}
+		}
+	} else {
+		fmt.Printf("\nUnable to determine canonical format; releases are unfiltered.\n\n")
+	}
+
+	return releases[0].Media[0].Tracks
+}
+
+func getCanonicalFormat(releases []model.Release) (format string, tracks string, err error) {
+	fmt.Printf("Analyzing formats..\n\n")
+
 	// count the number of releases for each combination of media and track counts
 	mediaCounts := make(map[string]int)
 
 	for _, release := range releases {
 		media, tracks := release.MediaInfo()
-		mediaCounts[media+", "+tracks]++
+		mediaCounts[media+":"+tracks]++
 	}
 
 	// sort the map by descending number of occurances
@@ -216,16 +245,42 @@ func getTrackList(mbid string) (tracks []model.Track) {
 		return mediaCountSlice[i].Value > mediaCountSlice[j].Value
 	})
 
-	// display the found formats
+	// determine the max format string length for spacing purposes
+	// initialize to the length of "format"
+	maxLen := 6
+	for _, format := range mediaCountSlice {
+		len := len(strings.Split(format.Key, ":")[0])
+		if len > maxLen {
+			maxLen = len
+		}
+	}
+
+	// check to see if the count of the top format matches the next, indicating
+	// an inconclusive match
+	inconclusive := false
+
+	if len(mediaCountSlice) > 1 && mediaCountSlice[0].Value == mediaCountSlice[1].Value {
+		inconclusive = true
+	}
+
 	for index, kv := range mediaCountSlice {
 		prefix := "   "
 
-		if index == 0 {
+		if !inconclusive && index == 0 {
 			prefix = "-->"
 		}
 
-		fmt.Printf("%s %s %d\n", prefix, kv.Key, kv.Value)
+		parts := strings.Split(kv.Key, ":")
+
+		fmt.Printf("%s %3dx\t%-*s\t%s\n", prefix, kv.Value, maxLen, parts[0], parts[1])
 	}
 
-	return releases[0].Media[0].Tracks
+	if inconclusive {
+		return "", "", errors.New("unable to determine canonical format")
+	}
+
+	bestFormat := mediaCountSlice[0].Key
+	bestFormatParts := strings.Split(bestFormat, ":")
+
+	return bestFormatParts[0], bestFormatParts[1], nil
 }
