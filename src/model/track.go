375ab2b89947e000dca932d26495b28594354381
JP Dillingham <jpdillingham@gmail.com>
2019-03-10 17:46:59 -0500

    add AlternateTitles to Track

--- src/model/track.go
+++ src/model/track.go
@@ -1,11 +1,12 @@
 package model
 
 type Track struct {
-	Position  int       `json:"position"`
-	ID        string    `json:"id"`
-	Length    int       `json:"length"`
-	Title     string    `json:"title"`
-	Number    string    `json:"number"`
-	Recording Recording `json:"recording"`
-	Score     float64   `json:"score"`
+	Position        int       `json:"position"`
+	ID              string    `json:"id"`
+	Length          int       `json:"length"`
+	Title           string    `json:"title"`
+	Number          string    `json:"number"`
+	Recording       Recording `json:"recording"`
+	Score           float64   `json:"score"`
+	AlternateTitles []string  `json:"alternate-titles"`
 }
